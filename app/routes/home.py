90cfdab7f1900ce7dff3c1c256428bec32cd291e
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2023-12-19 11:13:52 +0100

    aggiunto html home

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,11 +1,7 @@
 from flask import Blueprint, render_template
-
+import datetime
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 def index():
-    return "home"
-
-@home_bp.route('/about')
-def about():
-    return "about"
+    return render_template('home/home.html', ora=datetime.datetime.now().strftime("%H:%M:%S"))
