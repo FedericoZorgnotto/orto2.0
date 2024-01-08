1cd67f72bf434044458af5419827a086be83be4e
Luca Edmondo <l.edmondo.2899@vallauri.edu>
2024-01-08 10:18:21 +0100

    creato meccanismo di login lato client, e possibilità di importare automaticamente da js librerie e fogli di stile

--- app/static/scripts/register.js
+++ app/static/scripts/register.js
@@ -1,14 +1,21 @@
-document.getElementById("sendRegister").onclick = function (){
+
+$("#sendRegister").click(()=> {
   console.log("dsd");
   let body = ({
     email: $("#email").val(),
     password: sha1($("#password").val()),
     username: $("#username").val()
   })
-    sendReq("post", null, body, (result => {
-      console.log(result);
-      if(result.message == "utente già esistente"){
-        login(body.username, body.password);
-      }
-    }));
-};
+  sendReq("post", null, body, (result => {
+    console.log(result);
+    if (result.message == "successo") {
+      login(body.username, body.password);
+    } else if (result.message == "utente già esistente") {
+      location.href = location.href;
+      alert("nome utente già registrato");
+      console.warn("utente già esistente");
+    }
+  }));
+});
+
+function a(){}
