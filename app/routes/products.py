ae963da5f11b6c8111ed280d059117b3bdb536c2
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2024-01-13 23:17:51 +0100

    aggiunta route products.py, research e sell

--- app/routes/products.py
+++ app/routes/products.py
@@ -5,4 +5,27 @@ products_bp = Blueprint('products', __name__)
 
 @products_bp.route('/')
 def index():
-    return "gaga"
+    return redirect(url_for('products.research'))
+
+
+@products_bp.route('/sell', methods=['GET', 'POST'])
+def sell():
+    if request.method == 'GET':
+        return render_template('products/sell.html')
+    else:
+        return jsonify({'message': 'ok'})
+
+
+@products_bp.route('/research', methods=['GET', 'POST'])
+def research():
+    """
+    params: name, description, price, quantity, category
+    casi:
+    - andato a buon fine -> return success code, url home
+    - name già esistente -> return error code (name already exists)
+    - errore interno -> return error code (internal error)
+    """
+    if request.method == 'GET':
+        return render_template('products/research.html')
+    else:
+        return jsonify({'message': 'ok'})
