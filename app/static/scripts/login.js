ccceb22aa6e618e58bb2e8a73c5e15c35417d957
Luca Edmondo <l.edmondo.2899@vallauri.edu>
2024-01-14 12:02:34 +0100

    sistemato problema .click() in login e register

--- app/static/scripts/login.js
+++ app/static/scripts/login.js
@@ -1,38 +1,37 @@
-$("#enter-Account").click(() => {
-    alert("ciao");
+$(document).ready(()=>{
+    $("#enter-Account").click(() => {
+        let error = false;
 
-    let error = false;
+        if ($("#userEmail").val() == "") {
+            document.getElementById("emailError").innerText = "Write something";
+            document.getElementById("emailError").classList.remove("hide");
+            error = true;
+        } else {
+            document.getElementById("emailError").classList.add("hide");
+        }
 
-    if ($("#userEmail").val() == "") {
-        document.getElementById("emailError").innerText = "Write something";
-        document.getElementById("emailError").classList.remove("hide");
-        error = true;
-    } else {
-        document.getElementById("emailError").classList.add("hide");
-    }
+        if ($("#userPassword").val() == "") {
+            document.getElementById("passwordError").innerText = "Write something";
+            document.getElementById("passwordError").classList.remove("hide");
+            error = true;
+        } else {
+            document.getElementById("passwordError").classList.add("hide");
+        }
 
-    if ($("#userPassword").val() == "") {
-        document.getElementById("passwordError").innerText = "Write something";
-        document.getElementById("passwordError").classList.remove("hide");
-        error = true;
-    } else {
-        document.getElementById("passwordError").classList.add("hide");
-    }
-
-    if (!error) {
-        criptLogin($("#userEmail").val(), $("#userPassword").val(), (result) => {
-            if (result.message == "successo") {
-                localStorage.setItem("sessionUn", body.username);
-                localStorage.setItem("sessionPwd", body.password);
-            } else if (result.message == "password errata") {
-                document.getElementById("passwordError").classList.remove("hide");
-            } else if (result.message == "utente non trovato") {
-                document.getElementById("emailError").classList.remove("hide");
-            }
-        });
-    }
-});
-
+        if (!error) {
+            criptLogin($("#userEmail").val(), $("#userPassword").val(), (result) => {
+                if (result.message == "successo") {
+                    localStorage.setItem("sessionUn", body.username);
+                    localStorage.setItem("sessionPwd", body.password);
+                } else if (result.message == "password errata") {
+                    document.getElementById("passwordError").classList.remove("hide");
+                } else if (result.message == "utente non trovato") {
+                    document.getElementById("emailError").classList.remove("hide");
+                }
+            });
+        }
+    });
+})
 
 /*
     implementare l'add per riempire il database, fare richesta impost allo stesso indirizzo come request form, forse no risposte
--- app/static/scripts/register.js
+++ app/static/scripts/register.js
@@ -1,58 +1,57 @@
-$("#create-Account").click(() => {
-    let error = false;
+$(document).ready(()=>{
+    $("#create-Account").click(() => {
+        let error = false;
 
-    if ($("#userName").val() == "") {
-        document.getElementById("nameError").classList.remove("hide");
-        error = true;
-    } else {
-        document.getElementById("nameError").classList.add("hide");
-    }
-    if ($("#userSurname").val() == "") {
-        document.getElementById("surnameError").classList.remove("hide");
-        error = true;
-    } else {
-        document.getElementById("surnameError").classList.add("hide");
-    }
-    if ($("#userHandle").val() == "") {
-        document.getElementById("handleError").classList.remove("hide");
-        error = true;
-    } else {
-        document.getElementById("handleError").classList.add("hide");
-    }
-    if ($("#userEmail").val() == "") {
-        document.getElementById("emailError").classList.remove("hide");
-        error = true;
-    } else {
-        document.getElementById("emailError").classList.add("hide");
-    }
-    if (($("#userPassword").val()).length < 8) {
-        document.getElementById("passwordError").classList.remove("hide");
-        error = true
-    } else {
-        document.getElementById("passwordError").classList.add("hide");
-    }
+        if ($("#userName").val() == "") {
+            document.getElementById("nameError").classList.remove("hide");
+            error = true;
+        } else {
+            document.getElementById("nameError").classList.add("hide");
+        }
+        if ($("#userSurname").val() == "") {
+            document.getElementById("surnameError").classList.remove("hide");
+            error = true;
+        } else {
+            document.getElementById("surnameError").classList.add("hide");
+        }
+        if ($("#userHandle").val() == "") {
+            document.getElementById("handleError").classList.remove("hide");
+            error = true;
+        } else {
+            document.getElementById("handleError").classList.add("hide");
+        }
+        if ($("#userEmail").val() == "") {
+            document.getElementById("emailError").classList.remove("hide");
+            error = true;
+        } else {
+            document.getElementById("emailError").classList.add("hide");
+        }
+        if (($("#userPassword").val()).length < 8) {
+            document.getElementById("passwordError").classList.remove("hide");
+            error = true
+        } else {
+            document.getElementById("passwordError").classList.add("hide");
+        }
 
-    if (!error) {
-        console.log("tutto corretto")
-        let body = ({
-            email: $("#userEmail").val(),
-            password: sha1($("#userPassword").val()),
-            username: $("#userHandle").val(),
-            name: $("#userName").val(),
-            surname: $("#userSurname").val(),
-        })
-        sendReq("post", null, body, (result => {
-            if (result.message == "successo") {
-                location.href = result.url;
-            } else if (result.message == "utente già esistente") {
-                document.getElementById("handleError").innerText = "Already in use, sorry";
-            } else if (result.message == "mail già in uso") {
-                document.getElementById("emailError").innerText = "There is an other account connected";
-            }
-        }));
-    }
+        if (!error) {
+            console.log("tutto corretto")
+            let body = ({
+                email: $("#userEmail").val(),
+                password: sha1($("#userPassword").val()),
+                username: $("#userHandle").val(),
+                name: $("#userName").val(),
+                surname: $("#userSurname").val(),
+            })
+            sendReq("post", null, body, (result => {
+                if (result.message == "successo") {
+                    location.href = result.url;
+                } else if (result.message == "utente già esistente") {
+                    document.getElementById("handleError").innerText = "Already in use, sorry";
+                } else if (result.message == "mail già in uso") {
+                    document.getElementById("emailError").innerText = "There is an other account connected";
+                }
+            }));
+        }
 
-});
-
-function a() {
-}
+    });
+})
